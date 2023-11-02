840df986e93aa8f581bdf9954c03392afb1c3971
Qlrl <kiella999@kw.ac.kr>
2023-11-02 23:47:02 +0900

    v0.0.2

--- lib.js
+++ lib.js
@@ -3,11 +3,31 @@ function avg(numbers) {
     return s / numbers.length;
 }
 function prime(num) {
-    return false;
+    if (num < 2) {
+        return false; // Num is less than 2 are not prime
+    }
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) {
+            return false; // It's divisible by a number other than 1 and itself
+        }
+    }
+    return true; // It's prime
 }
+
 function fact(num) {
-    return -1;
+    if (num < 0) {
+        return "num is 0 이상이에요~"; // You can also return an error message here
+    }
+    if (num > 15) {
+        return "num is 15이하의 자연수에요~";
+    }
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+        result *= i;
+    }
+    return result;
 }
+
 
 module.exports = {
     avg,
--- lib.test.js
+++ lib.test.js
@@ -4,7 +4,18 @@ const lib = require("./lib");
 test("avg([3, 5, 7]) should be 5", () => {
     expect(lib.avg([3, 5, 7])).toBe(5);
 });
-
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
+});
+test("prime[3] should be true", () => {
+    expect(lib.prime([3])).toBe(true);
+});
+test("prime[100] should be false", () => {
+    expect(lib.prime([100])).toBe(false);
+});
+test("fact[3] should be 6", () => {
+    expect(lib.fact([3])).toBe(6);
+});
+test("fact[10] should be 3628800", () => {
+    expect(lib.fact([10])).toBe(3628800);
 });
