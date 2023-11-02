d3937ee767ffd35ac24de078a9e277c9a9d2fd4d
Qlrl <kiella999@kw.ac.kr>
2023-11-02 20:37:58 +0900

    0.0.1 version commit in github

--- lib.js
+++ lib.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-function avg(numbers) {
-    let s = numbers.reduce((prev, curr) => prev + curr, 0);
-    return s / numbers.length;
-}
-function prime(num) {
-    if (num < 2) {
-        return false; // Numbers less than 2 are not prime
-    }
-    for (let i = 2; i * i <= num; i++) {
-        if (num % i === 0) {
-            return false; // It's divisible by a number other than 1 and itself
-        }
-    }
-    return true; // It's prime
-
-}
-function fact(num) {
-    if (num > 15) {
-        return "15보다 큰 수는 입력할 수 없습니다.";
-    }
-    if (num < 0) {
-        return "음수는 입력할 수 없습니다."; // You can also return an error message here
-    }
-    let result = 1;
-    for (let i = 1; i <= num; i++) {
-        result *= i;
-    }
-    return result;
-}
-module.exports = {
-    avg,
-    prime,
-    fact
-}
-=======
 function avg(numbers) {
     let s = numbers.reduce((prev, curr) => prev + curr, 0);
     return s / numbers.length;
@@ -44,9 +8,10 @@ function prime(num) {
 function fact(num) {
     return -1;
 }
+
 module.exports = {
     avg,
     prime,
     fact
 }
->>>>>>> e454d3d84d808d6f6e3976ca31b5eadbc2469bb8
+
--- lib.test.js
+++ lib.test.js
@@ -1,30 +1,10 @@
-<<<<<<< HEAD
 const { test, expect } = require("@jest/globals");
 const lib = require("./lib");
+
 test("avg([3, 5, 7]) should be 5", () => {
     expect(lib.avg([3, 5, 7])).toBe(5);
 });
+
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
-});
-test("prime[3] should be true", () => {
-    expect(lib.prime([3])).toBe(true);
-});
-test("prime[100] should be false", () => {
-    expect(lib.prime([100])).toBe(false);
-});
-test("fact[3] should be 6", () => {
-    expect(lib.fact([3])).toBe(6);
-});
-test("fact[10] should be 3628800", () => {
-    expect(lib.fact([10])).toBe(3628800);
-=======
-const { test, expect } = require("@jest/globals");
-const lib = require("./lib");
-test("avg([3, 5, 7]) should be 5", () => {
-    expect(lib.avg([3, 5, 7])).toBe(5);
-});
-test("avg([-5, 5]) should be 0", () => {
-    expect(lib.avg([-5, 5])).toBe(0);
->>>>>>> e454d3d84d808d6f6e3976ca31b5eadbc2469bb8
 });
