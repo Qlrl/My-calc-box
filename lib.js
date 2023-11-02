305b71d051cf2ab9736214596640ff6c992987c1
Qlrl <kiella999@kw.ac.kr>
2023-11-02 17:23:52 +0900

    commit to github(second try)

--- lib.js
+++ lib.js
@@ -3,10 +3,29 @@ function avg(numbers) {
     return s / numbers.length;
 }
 function prime(num) {
-    return false;
+    if (num < 2) {
+        return false; // Numbers less than 2 are not prime
+    }
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) {
+            return false; // It's divisible by a number other than 1 and itself
+        }
+    }
+    return true; // It's prime
+
 }
 function fact(num) {
-    return -1;
+    if (num > 15) {
+        return "15보다 큰 수는 입력할 수 없습니다.";
+    }
+    if (num < 0) {
+        return "음수는 입력할 수 없습니다."; // You can also return an error message here
+    }
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+        result *= i;
+    }
+    return result;
 }
 module.exports = {
     avg,
